f8c0ff9987c646540d44fe4db15a770ff27e77af
Maesro <maestrerodrigo@gmail.com>
2024-05-30 20:50:17 -0300

    Se sube caso de ejemplo

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://www.google.com" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+          <p>React</p>
+        </a>
+      </div>
+      <h1>Hola Mundo2</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Hola
+      </p>
+    </>
+  )
+}
+
+export default App
